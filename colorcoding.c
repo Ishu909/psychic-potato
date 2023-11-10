a846115b89ee5144537a661559285793a06de1ee
Ishu909 <116420323+Ishu909@users.noreply.github.com>
2023-11-10 14:46:39 +0530

    Update colorcoding.c

--- colorcoding.c
+++ colorcoding.c
@@ -7,15 +7,15 @@ void player2(char str1[])
 {
 	int i,j,k,flag=0,right=0,wrong=0,rightbutwrong=0,c=1;
 	char str2[100];
-	for(i=0;i<5;i++)
+	for(i=0;i<8;i++)
 	{	
 		right=0;
 		wrong=0;
 		rightbutwrong=0;
 		printf("\n");
 		printf("CHANCES left : ");
-		for(int z=5; z>i; z--)
-			printf("\3 ");
+		for(int z=8; z>i; z--)
+			printf("|");
 		printf("\n");
 		scanf("%s", str2);
 		for(j=0; j<strlen(str1); j++)
@@ -69,7 +69,7 @@ void intro()
 {
 	printf("\3CODE\3GUESSING\3");
 	printf("\n---------------------------------------------------------\5INSTRUCTIONS\5-----------------------------------------------------------\n");
-	printf("\4 THIS GAME CONSIST OF TWO PLAYERS->PLAYER1 AND PLAYER2.\n\4 PLAYER1 WRITE THE CODE WHICH IS UNKNOWN TO PLAYER2 NOW PLAYER TWO WILL HAVE 5 CHANCES TO GUESS THE CORRECT CODE\n\4IF PLAYER TWO GUESS THE CORRECT CODE IN 5 CHANCES THEN HE/SHE WILL WIN THE MATCH\n\4 IF PLAYER2 NOT ABLE TO FIND THE CODE IN 8 CHANCE THEN HE/SHE WILL LOSE THE GAME.\n");
+	printf("\4 THIS GAME CONSIST OF TWO PLAYERS->PLAYER1 AND PLAYER2.\n\4 PLAYER1 WRITE THE CODE WHICH IS UNKNOWN TO PLAYER2 NOW PLAYER TWO WILL HAVE 8 CHANCES TO GUESS THE CORRECT CODE\n\4IF PLAYER TWO GUESS THE CORRECT CODE IN 8 CHANCES THEN HE/SHE WILL WIN THE MATCH\n\4 IF PLAYER2 NOT ABLE TO FIND THE CODE IN 8 CHANCE THEN HE/SHE WILL LOSE THE GAME.\n");
 	printf("---------------------------------------------------------GAME HAS BEEN STARTED--------------------------------------------------");
 	printf("\nENTER THE CODE TO HIDE : \n");
 	return ;
